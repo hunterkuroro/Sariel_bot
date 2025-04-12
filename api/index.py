be289d956b0b9b6d6fc779db36a54b39d37904b1
hunterkuroro <eponinefang@gmail.com>
2025-04-13 05:26:45 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -39,18 +39,24 @@ def handle_message(event):
     if event.message.type != "text":
         return
 
-    if event.message.text == "說話":
+    if event.message.text == "早安":
         working_status = True
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
+     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 格式化當前時間，可根據需求修改格式
+    reply_text = f" {now}"
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)
+    )
         return
 
-    if event.message.text == "閉嘴":
+    if event.message.text == "再見":
         working_status = False
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="好的，我乖乖閉嘴 > <，如果想要我繼續說話，請跟我說 「說話」 > <"))
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 格式化當前時間，可根據需求修改格式
+    reply_text = f" {now}"
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)
+    )
         return
 
     # For ChatGpt
