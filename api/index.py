30c50fd7bfeb608164c9d2963cffa78d7a6588b2
hunterkuroro <eponinefang@gmail.com>
2025-04-13 04:06:28 +0800

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,37 +1,34 @@
+
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from api.chatgpt import ChatGPT
+from chatgpt_with_context_Michael import chat_with_gpt  # 🚀 加入角色語氣 GPT 回應模組
 
 import os
+import asyncio
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+working_status = os.getenv("DEFALUT_TALKING", default="true").lower() == "true"
 
 app = Flask(__name__)
-chatgpt = ChatGPT()
 
-# domain root
 @app.route('/')
 def home():
     return 'Hello, World!'
 
 @app.route("/webhook", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -39,29 +36,29 @@ def handle_message(event):
     if event.message.type != "text":
         return
 
-    if event.message.text == "說話":
+    text = event.message.text
+    user_id = event.source.user_id
+
+    if text == "說話":
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
         return
 
-    if event.message.text == "閉嘴":
+    if text == "閉嘴":
         working_status = False
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="好的，我乖乖閉嘴 > <，如果想要我繼續說話，請跟我說 「說話」 > <"))
         return
 
-    # For ChatGpt
     if working_status:
-        chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
-        reply_msg = chatgpt.get_response().replace("AI:", "", 1)
-        chatgpt.add_msg(f"AI:{reply_msg}\n")
+        # 使用角色語氣回應
+        reply_msg = asyncio.run(chat_with_gpt(user_id, text))
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
-
 
 if __name__ == "__main__":
     app.run()
