31357b00093926e3b0afd2b8917ab5f42332c9a4
hunterkuroro <eponinefang@gmail.com>
2025-04-13 05:47:07 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -39,25 +39,25 @@ def handle_message(event):
     if event.message.type != "text":
         return
 
-    if event.message.text == "早安":
-        working_status = True
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 格式化當前時間，可根據需求修改格式
-        reply_text = f" {now}"
-        line_bot_api.reply_message(
+   if event.message.text == "早安":
+    working_status = True
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 格式化當前時間，可根據需求修改格式
+    reply_text = f"{now}"
+    line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text)
     )
-        return
+    return
 
-    if event.message.text == "再見":
-        working_status = False
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 格式化當前時間，可根據需求修改格式
-        reply_text = f" {now}"
-        line_bot_api.reply_message(
+if event.message.text == "再見":
+    working_status = False
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 格式化當前時間，可根據需求修改格式
+    reply_text = f"{now}"
+    line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text)
     )
-        return
+    return
 
     # For ChatGpt
     if working_status:
